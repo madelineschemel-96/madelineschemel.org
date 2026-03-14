bdd2fc104cc6cbd0e904ef3c80c88d13c24e6fef
madelineschemel-96 <madelinevanhoose@gmail.com>
2026-03-14 16:41:57 -0500

    Updates

---
+++ artifacts/hub-start-storyline/New Business_ Adding a Prospect/story_content/6ALVBiomqba_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[]}]};
+    window.globalLoadJsAsset('story_content/6ALVBiomqba_transcripts.js', JSON.stringify(data));
+})();
